fa8ca414bb39e4080680dbcc661da8a724a7ab27
windpupil <125351252+windpupil@users.noreply.github.com>
2025-01-25 10:55:32 +0800

    大三上更新

---
+++ 数据库/Test3/Form1.Designer.cs
@@ -0,0 +1,279 @@
+﻿namespace Test3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(51, 39);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(62, 18);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "label1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(51, 95);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(62, 18);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "label2";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(119, 92);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 28);
+            this.textBox1.TabIndex = 2;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(246, 95);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(62, 18);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "label3";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(323, 92);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(143, 28);
+            this.textBox2.TabIndex = 4;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(492, 95);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(62, 18);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "label4";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(572, 92);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(179, 28);
+            this.textBox3.TabIndex = 6;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(789, 95);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(62, 18);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "label5";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(875, 92);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(57, 26);
+            this.comboBox1.TabIndex = 8;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(54, 153);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(62, 18);
+            this.label6.TabIndex = 9;
+            this.label6.Text = "label6";
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(165, 153);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(121, 26);
+            this.comboBox2.TabIndex = 10;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(390, 153);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(115, 22);
+            this.checkBox1.TabIndex = 11;
+            this.checkBox1.Text = "checkBox1";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(54, 201);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(100, 37);
+            this.button1.TabIndex = 12;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(211, 201);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(97, 37);
+            this.button2.TabIndex = 13;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(377, 201);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(103, 37);
+            this.button3.TabIndex = 14;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(541, 201);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(96, 37);
+            this.button4.TabIndex = 15;
+            this.button4.Text = "button4";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(691, 201);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(97, 37);
+            this.button5.TabIndex = 16;
+            this.button5.Text = "button5";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(834, 201);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(98, 37);
+            this.button6.TabIndex = 17;
+            this.button6.Text = "button6";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(54, 270);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 62;
+            this.dataGridView1.RowTemplate.Height = 30;
+            this.dataGridView1.Size = new System.Drawing.Size(863, 399);
+            this.dataGridView1.TabIndex = 18;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(967, 715);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.DataGridView dataGridView1;
+    }
+}
+
---
+++ 数据库/Test3/Form1.cs
@@ -0,0 +1,216 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Test3
+{
+    public partial class Form1 : Form
+    {
+        string connetStr ="server=localhost;port=3306;user = root;password=123456;database=test";
+        MySqlConnection myconnection;
+        public Form1()
+        {
+            InitializeComponent();
+            Form1_Load(null, null);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            label1.ForeColor = Color.Green;
+            label1.Text = "操作状态";
+
+            label2.Text = "姓名";
+            label3.Text = "邮箱";
+            label4.Text = "电话";
+            label5.Text = "性别";
+
+            textBox1.Name = "tB_Name";
+            textBox2.Name = "tB_Mail";
+            textBox3.Name = "tB_Phone";
+            comboBox1.Name = "cB_Sex";
+            comboBox1.Items.Add("男");
+            comboBox1.Items.Add("女");
+
+            label6.Text = "查询条件";
+            comboBox2.Name = "cB_tj";
+            comboBox2.Items.Add("全部");
+            comboBox2.Items.Add("姓名");
+            comboBox2.Items.Add("邮箱");
+            comboBox2.Items.Add("电话");
+            comboBox2.Items.Add("性别");
+            checkBox1.Name = "chk_mode";
+            checkBox1.Text = "模糊查询";
+
+            button1.Text = "连接";
+            button1.Name = "btConnect";
+            button2.Text = "查询";
+            button2.Name = "btQueryAll";
+            button3.Text = "插入";
+            button3.Name = "btInsertToDatabase";
+            button4.Text = "修改";
+            button4.Name = "btModifiFeildValue";
+            button5.Text = "删除";
+            button5.Name = "btDeleteFeildValue";
+            button6.Text = "断开连接";
+            button6.Name = "btDisConnect";
+        }
+        //连接按钮
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                myconnection = new MySqlConnection(connetStr);
+                myconnection.Open();
+                label1.Text = "数据库连接成功";
+            }
+            catch (MySqlException ex)
+            {
+                MessageBox.Show("数据库连接失败" + ex.ToString());
+            }
+        }
+
+        public string DisConnect()
+        {
+            try
+            {
+                myconnection.Close();
+                return "数据库断开成功";
+            }
+            catch (MySqlException ex)
+            {
+                return "数据库断开失败" + ex.ToString();
+            }
+        }   
+        //断开连接按钮
+        private void button6_Click(object sender, EventArgs e)
+        {
+            label1.Text = DisConnect();
+        }
+
+        //插入按钮
+        private void button3_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string strSQL1,strname,strmail,strphone,strsex;
+                strname = textBox1.Text;
+                strmail = textBox2.Text;
+                strphone = textBox3.Text;
+                strsex = comboBox1.Text;
+                strSQL1 = String.Format("insert into TXL values('{0}','{1}','{2}','{3}')", strname, strmail, strphone, strsex);
+
+                MySqlDataAdapter objDataAdpter = new MySqlDataAdapter();
+                MySqlCommand thisCommand = new MySqlCommand(strSQL1, myconnection);
+                thisCommand.ExecuteNonQuery();
+
+                string strSQL2 = "select * from TXL";
+                MySqlDataAdapter objDataAdpter1 = new MySqlDataAdapter();
+                objDataAdpter1.SelectCommand = new MySqlCommand(strSQL2, myconnection);
+                DataSet ds = new DataSet();
+                objDataAdpter1.Fill(ds);
+                dataGridView1.DataSource = ds.Tables[0];
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("插入失败" + ex.ToString());
+            }
+        }
+        //查询按钮
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string strSQL = "";
+            try
+            {        
+                switch(comboBox2.Text)
+                {
+                    case "全部":
+                        strSQL = "select * from TXL";
+                        break;
+                    case "姓名":
+                        if (checkBox1.Checked)
+                            strSQL = String.Format("select * from TXL where name like '%{0}%'", textBox1.Text);
+                        else
+                            strSQL = String.Format("select * from TXL where name = '{0}'", textBox1.Text);
+                        break;
+                    case "邮箱":
+                        if (checkBox1.Checked)
+                            strSQL = String.Format("select * from TXL where mail like '%{0}%'", textBox2.Text);
+                        else
+                            strSQL = String.Format("select * from TXL where mail = '{0}'", textBox2.Text);
+                        break;
+                    case "电话":
+                        if (checkBox1.Checked)
+                            strSQL = String.Format("select * from TXL where phone like '%{0}%'", textBox3.Text);
+                        else
+                            strSQL = String.Format("select * from TXL where phone = '{0}'", textBox3.Text);
+                        break;
+                    case "性别":
+                        if (checkBox1.Checked)
+                            strSQL = String.Format("select * from TXL where sex like '%{0}%'", comboBox1.Text);
+                        else
+                            strSQL = String.Format("select * from TXL where sex = '{0}'", comboBox1.Text);
+                        break;
+                }
+                MySqlCommand cmd =new MySqlCommand(strSQL, myconnection);
+                MySqlDataAdapter adpt1 = new MySqlDataAdapter(cmd);
+
+                DataSet ds = new DataSet();
+                adpt1.Fill(ds);
+                dataGridView1.DataSource = ds.Tables[0];
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("查询失败" + ex.ToString());
+            }
+        }
+        //修改按钮
+        private void button4_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string strSQL1 = string.Format("update TXL set mail='{0}',phone='{1}',sex='{2}' where name='{3}'", textBox2.Text, textBox3.Text, comboBox1.Text, textBox1.Text);
+                MySqlCommand thisCommand = new MySqlCommand(strSQL1, myconnection);
+                thisCommand.ExecuteNonQuery();
+
+                string strSQL2 = "select * from TXL";
+                MySqlDataAdapter objDataAdpter1 = new MySqlDataAdapter();
+                objDataAdpter1.SelectCommand = new MySqlCommand(strSQL2, myconnection);
+                DataSet ds = new DataSet();
+                objDataAdpter1.Fill(ds, "TXL");
+                dataGridView1.DataSource = ds.Tables[0];
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("修改失败" + ex.ToString());
+            }
+        }
+        //删除按钮
+        private void button5_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string strSQL1 = string.Format("delete from TXL where name='{0}'", textBox1.Text);
+                MySqlCommand thisCommand = new MySqlCommand(strSQL1, myconnection);
+                thisCommand.ExecuteNonQuery();
+
+                string strSQL2 = "select * from TXL";
+                MySqlDataAdapter objDataAdpter1 = new MySqlDataAdapter();
+                objDataAdpter1.SelectCommand = new MySqlCommand(strSQL2, myconnection);
+                DataSet ds = new DataSet();
+                objDataAdpter1.Fill(ds, "TXL");
+                dataGridView1.DataSource = ds.Tables[0];
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("删除失败" + ex.ToString());
+            }
+        }
+    }
+}
